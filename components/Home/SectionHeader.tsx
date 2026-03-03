9b4246b455e505ec6966f2a1bec0eaad3012034f
Samuel Echefu <samuelnnechefu@gmail.com>
2026-03-02 17:28:25 -0800

    Touching up home

--- components/Home/SectionHeader.tsx
+++ components/Home/SectionHeader.tsx
@@ -1,12 +1,31 @@
 "use client";
 
+import { redirect } from "next/navigation";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
-const SectionHeader = ({title}: {title: string}) => {
+const SectionHeader = ({
+  title,
+  type,
+}: {
+  title: string;
+  type: "listings" | "messages" | "null";
+}) => {
   return (
     <div className="flex justify-between gap-2">
       <h3 className="text-2xl">{title}</h3>
-      <button className="cursor-pointer">
+      <button
+        onClick={() => {
+          switch (type) {
+            case "listings":
+              redirect("/listings");
+            case "messages":
+              redirect("/conversations");
+            default:
+              break;
+          }
+        }}
+        className="cursor-pointer"
+      >
         <MdKeyboardArrowRight className="text-3xl" />
       </button>
     </div>

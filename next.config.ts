768ed9321c49f9ce09278f797f52e81ea8199472
Samuel Echefu <samuelnnechefu@gmail.com>
2026-09-15 14:26:50 -0700

    oh mann

--- next.config.ts
+++ next.config.ts
@@ -33,32 +33,8 @@ const nextConfig: NextConfig = {
     },
   },
   reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        // This targets all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          // Allows your Expo web client on port 3000 to fetch data
-          {
-            key: "Access-Control-Allow-Origin",
-            value:
-              "https://app.market-quad.com, https://market-quad.com, https://web.market-quad.com",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, x-cloud-folder",
-          },
-        ],
-      },
-    ];
-  },
+  
+  
 };
 
 export default nextConfig;
